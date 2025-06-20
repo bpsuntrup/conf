ae39ab360a2b43b783f282ca75d680cce521f546
Benjamin Paul Suntrup <bpsxv3@mst.edu>
2025-06-20 10:38:54 -0500

    nvim: add new mappings for nvim-tree

--- nvim/init.lua
+++ nvim/init.lua
@@ -82,6 +82,24 @@ require('lazy').setup({
         filters = {
           git_ignored = false,
         },
+
+        on_attach = function(bufnr)
+          local api = require 'nvim-tree.api'
+          local function opts(desc)
+            return {
+              desc = 'nvim-tree: ' .. desc,
+              buffer = bufnr,
+              noremap = true,
+              silent = true,
+              nowait = true,
+            }
+          end
+
+          api.config.mappings.default_on_attach(bufnr)
+          vim.keymap.set('n', '?', api.tree.toggle_help, opts 'Help')
+          vim.keymap.set('n', 'y', api.fs.copy.node, opts 'Copy')
+          vim.keymap.set('n', 'c', api.node.navigate.parent_close, opts 'Close')
+        end,
       }
     end,
   },
